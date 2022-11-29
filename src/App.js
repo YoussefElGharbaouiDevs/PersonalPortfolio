e973f5b8f9f36c43429bdf0637d60c9759ea4cda
Youssef el gharbaoui <youssefelgharbaoui.pro@gmail.com>
2022-11-29 20:53:01 +0100

    Adding the banner section to the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './components/header'
+import Banner from './components/banner'
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banner />
     </div>
   );
 }
